fe6a123c19f68cac560c7617316c353d64e714ac
Lada Fomina <ladafmn@gmail>
2018-08-22 13:19:08 +0700

    Сделала калькулятор На калькуляторе работают цифры

--- main.js
+++ main.js
@@ -15,42 +15,13 @@ enterNumber = (n) => {
     }
 };
 
-const num1 = document.getElementById('num1');
-num1.onclick = () => {
-    enterNumber(1)
-};
-const num2 = document.getElementById('num2');
-num2.onclick = () => {
-    enterNumber(2)
-};
-const num3 = document.getElementById('num3');
-num3.onclick = () => {
-    enterNumber(3)
-};
-const num4 = document.getElementById('num4');
-num4.onclick = () => {
-    enterNumber(4)
-};
-const num5 = document.getElementById('num5');
-num5.onclick = () => {
-    enterNumber(5)
-};
-const num6 = document.getElementById('num6');
-num6.onclick = () => {
-    enterNumber(6)
-};
-const num7 = document.getElementById('num7');
-num7.onclick = () => {
-    enterNumber(7)
-};
-const num8 = document.getElementById('num8');
-num8.onclick = () => {
-    enterNumber(8)
-};
-const num9 = document.getElementById('num9');
-num9.onclick = () => {
-    enterNumber(9)
-};
+const numButtons = document.getElementsByClassName('button');
+
+for (let i = 0; i < numButtons.length; i++) {
+    const button = numButtons [i];
+    const num = button.textContent;
+    button.onclick = () => {enterNumber(num)}
+}
 
 
 //DOM - Document Object Model
